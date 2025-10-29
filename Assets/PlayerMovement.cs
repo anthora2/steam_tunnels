6d9217e13076da1a71a8cfdeea3a21e6dfd639c0
Anthony Alvarez <anthony.lvarez03@gmail.com>
2025-10-29 16:27:25 -0700

    Changed character into speed, put into first person, still a little buggy

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,62 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     [Header("Movement")]
-    public float walkSpeed = 3.5f;
-    public float runSpeed  = 7.0f;
-    public float rotationLerp = 12f; // higher = snappier turns
+    public float moveSpeed;
 
-    private Animator animator;
-    private Transform cam;
+    public float groundDrag;
 
-    void Awake()
-    {
-        animator = GetComponent<Animator>();           // needs an Animator on this object
-        cam = Camera.main != null ? Camera.main.transform : null;
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    void Update()
-    {
-        // --- 1) Read input ---
-        float x = Input.GetAxisRaw("Horizontal"); // A/D or Left/Right
-        float z = Input.GetAxisRaw("Vertical");   // W/S or Up/Down
-        Vector2 input = new Vector2(x, z);
-        bool hasInput = input.sqrMagnitude > 0.0001f;
+    private void Update() {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 1 + 0.2f, whatIsGround);
+        MyInput();
+        SpeedControl();
 
-        // --- 2) Choose walk vs run ---
-        bool running = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-        float targetSpeed = running ? runSpeed : walkSpeed;
-
-        // --- 3) Camera-relative movement vector (XZ only) ---
-        Vector3 moveDir;
-        if (cam != null)
-        {
-            Vector3 forward = cam.forward; forward.y = 0f; forward.Normalize();
-            Vector3 right   = cam.right;   right.y   = 0f; right.Normalize();
-            moveDir = (forward * z + right * x);
-        }
+        if (grounded) 
+            rb.linearDamping = groundDrag;
         else
-        {
-            // fallback if no camera found
-            moveDir = new Vector3(x, 0f, z);
+        rb.linearDamping = 0;
+    }
+
+    private void FixedUpdate() {
+        MovePlayer();
+    }
+
+    private void MyInput() {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer() {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl(){
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        if (flatVel.magnitude > moveSpeed) {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
         }
-        if (moveDir.sqrMagnitude > 1f) moveDir.Normalize();
-
-        // --- 4) Move ---
-        transform.position += moveDir * targetSpeed * Time.deltaTime;
-
-        // --- 5) Face movement direction ---
-        if (hasInput && moveDir.sqrMagnitude > 0.0001f)
-        {
-            Quaternion targetRot = Quaternion.LookRotation(moveDir, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationLerp * Time.deltaTime);
-        }
-
-        // --- 6) Drive Animator parameter ("speed" float you created) ---
-        // Normalize 0..1 based on runSpeed so Animator thresholds are easy to set
-        float normalizedSpeed = hasInput ? (targetSpeed / runSpeed) : 0f; // 0 idle, ~0.5 walk, 1 run
-        if (animator != null) animator.SetFloat("speed", normalizedSpeed, 0.1f, Time.deltaTime);
-        // (the last two args damp the change so blends are smooth)
     }
 }
